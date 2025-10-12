407078a8f7b21cfdfc58589ae051af374116ff0d
FidelisKagashe <fideliskagashe@gmail.com>
2025-10-12 14:36:33 +0300

    Next changes

--- godcares_backend/settings.py
+++ godcares_backend/settings.py
@@ -105,6 +105,10 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
 # Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
